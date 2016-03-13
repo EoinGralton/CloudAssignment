497487dc545eeccb7ef39b8a42e0a6f8432f86f2
L00131070 <L00131070@student.lyit.ie>
2016-03-13 19:52:37 +0000

    added SQL script for database in directory SQL

---
+++ SQL/schema.sql
@@ -0,0 +1,12 @@
+CREATE SCHEMA assignment;
+
+CREATE TABLE assignment.users ( 
+	id                   int(10)  NOT NULL  ,
+	email                varchar(50)  NOT NULL  ,
+	password             binary(20)  NOT NULL  ,
+	isadmin              int(10)  NOT NULL DEFAULT 0 ,
+	CONSTRAINT pk_users PRIMARY KEY ( id )
+ );
+
+INSERT INTO assignment.users( id, email, password, isadmin ) VALUES ( 2, 'test@test.com', '[B@2da23947', 0 ); 
+
