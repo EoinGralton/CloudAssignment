244d0ac6b73eed2653fc902a5d5fd1a633b46958
L00131070 <L00131070@student.lyit.ie>
2016-03-17 17:02:39 +0000

    updated sql schema script

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,40 +1,54 @@
-CREATE TABLE `policy` (
-  `idpolicy` int(11) NOT NULL,
-  `outsideEuUS` tinyint(1) DEFAULT NULL,
-  `cover_start` date DEFAULT NULL,
-  `cover_finish` date DEFAULT NULL,
-  `health_insurance` tinyint(1) DEFAULT NULL,
-  PRIMARY KEY (`idpolicy`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-CREATE TABLE `person` (
-  `idperson` int(11) NOT NULL,
-  `idpolicy` int(11) NOT NULL,
-  `admin` tinyint(1) DEFAULT NULL,
-  `title` varchar(5) DEFAULT NULL,
-  `FName` varchar(20) DEFAULT NULL,
-  `LName` varchar(30) DEFAULT NULL,
-  `D.O.B` date DEFAULT NULL,
-  `Email` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`idperson`),
-  KEY `idpolicy_idx` (`idpolicy`),
-  CONSTRAINT `idpolicy` FOREIGN KEY (`idpolicy`) REFERENCES `policy` (`idpolicy`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-CREATE TABLE `member` (
-  `email` varchar(15) DEFAULT NULL,
-  `password` varchar(13) DEFAULT NULL,
-  `idperson` int(11) DEFAULT NULL,
-  KEY `idperson_idx` (`idperson`),
-  CONSTRAINT `idperson` FOREIGN KEY (`idperson`) REFERENCES `person` (`idperson`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-CREATE TABLE `policyperson` (
-  `idPolicyPerson` int(11) NOT NULL,
-  `idpolicy` int(11) NOT NULL,
-  `Title` varchar(5) DEFAULT NULL,
-  `FName` varchar(20) DEFAULT NULL,
-  `LName` varchar(30) DEFAULT NULL,
-  `D.O.B` date DEFAULT NULL,
-  PRIMARY KEY (`idPolicyPerson`),
-  KEY `idpolicy_idx` (`idpolicy`),
-  KEY `idpolicyfk_idx` (`idpolicy`),
-  CONSTRAINT `idpolicyfk` FOREIGN KEY (`idpolicy`) REFERENCES `policy` (`idpolicy`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE assignment.member ( 
+	email                varchar(50)    ,
+	password             binary(20)  NOT NULL  ,
+	id                   int  NOT NULL  AUTO_INCREMENT,
+	CONSTRAINT idperson_idx UNIQUE ( id ) ,
+	CONSTRAINT pk_member PRIMARY KEY ( id ),
+	CONSTRAINT idx_member_email UNIQUE ( email ) 
+ );
+
+CREATE TABLE assignment.person ( 
+	id                   int  NOT NULL AUTO_INCREMENT ,
+	idmember             int    ,
+	admin                bit    ,
+	title                varchar(5)    ,
+	firstname            varchar(30)    ,
+	lastname             varchar(30)    ,
+	dateofbirth          date    ,
+	CONSTRAINT pk_person PRIMARY KEY ( id )
+ );
+
+CREATE TABLE assignment.policy ( 
+	id                   int  NOT NULL  AUTO_INCREMENT,
+	idperson             int    ,
+	`outsideEuUS`        bit  NOT NULL  ,
+	start                date    ,
+	finish               date    ,
+	health_insurance     bit    ,
+	CONSTRAINT pk_policy PRIMARY KEY ( id )
+ );
+
+CREATE INDEX idx_policy ON assignment.policy ( idperson );
+
+CREATE TABLE assignment.policyperson ( 
+	`idPolicyPerson`     int  NOT NULL AUTO_INCREMENT,
+	idpolicy             int  NOT NULL  ,
+	title                varchar(5)    ,
+	firstname            varchar(30)    ,
+	lastname             varchar(30)    ,
+	dateofbirth          date    ,
+	CONSTRAINT pk_policyperson PRIMARY KEY ( `idPolicyPerson` )
+ );
+
+CREATE INDEX idpolicyfk_idx ON assignment.policyperson ( idpolicy );
+
+CREATE VIEW assignment.personview AS SELECT person.*, member.email FROM person, member where person.idmember = member.id;
+
+ALTER TABLE assignment.person ADD CONSTRAINT fk_person_member FOREIGN KEY ( id ) REFERENCES assignment.member( id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE assignment.policy ADD CONSTRAINT fk_policy_person FOREIGN KEY ( idperson ) REFERENCES assignment.person( id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE assignment.policyperson ADD CONSTRAINT idpolicyfk FOREIGN KEY ( idpolicy ) REFERENCES assignment.policy( id ) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+INSERT INTO assignment.member( id, email, password ) VALUES ( 1, 'test@test.com', '[B@6ce27568' ); 
+
